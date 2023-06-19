310ddab879d3161c22d8b15397ced8438869d860
Alex1121111 <datta.pesala@gmail.com>
2023-06-19 23:13:02 +0530

    Added Styling for Button and text box

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ function App() {
         <section>
         <img className="logo" src= "logo.png"></img>
         </section>
+        <section className="header1">
+          <div className="info_container">
+          <h2>Build fast, light weight bots with AutoBot</h2>
+          </div>
+        </section>
+        <section className="header2">
+          <div className="info_container2">
+          <h2>Experience the ultimate frontend toolkit that empowers you to build stunning websites. Bring your vision to life with our comprehensive toolkit and take your web development to new heights!</h2>
+          </div>
+        </section>
+        <div className="textdiv">
+          <input type="text" className="textbox"></input>
+          <button className="btn btn-primary button-30">Build</button>
+        </div>
       </body>
     </div>
     
